2501d74e5bb87fe38780c2e226ed5753f9be6545
Morgan <gann.gann87@gmail.com>
2021-01-03 10:47:59 +0100

    Pré mise en place du routeur poo

--- public/app/routeur.php
+++ public/app/routeur.php
@@ -1,9 +1,39 @@
 <?php
 
-  /*
+/*
     ./app/routeur.php
     ROUTEUR PRINCIPAL
     Charge le contrôleur et lance l'action qui correspond à ce qui se passe dans l'URL
   */
 
-    include_once '../app/routeurs/public.php';
+// include_once '../app/routeurs/public.php';
+
+// var_dump($_GET);
+// $url = explode("index.php", $_GET['url']);
+// var_dump($url);
+// die;
+
+$router = new Noyau\Classes\Router($_GET['url']);
+
+$router->get('/', function () {
+  $ctrl = new App\Controleurs\PostsControleur();
+  $ctrl->indexAction(5, 0, 'created_at',"DESC");
+});
+// $router->get('/posts', function () {
+//   echo 'Tous les article';
+// });
+// die;
+
+$router->get('/posts/:id/:slug', function ($id, $slug) use ($router) {
+  $ctrl = new App\Controleurs\PostsControleur();
+  $ctrl->showAction($id);
+  // echo $router->url('posts.show', ['id' => 1, 'slug' => 'Salut-les-gens']);
+}, 'posts.show')->with('id', '[0-9]+')->with('slug', '[a-z\-0-9]+');
+
+
+// $router->get('/posts/:id', "Posts#show");
+// $router->post('/posts/:id', function ($id) {
+//   echo 'Poster l\'Article ' . $id . '<pre>' . print_r($_POST, true) . '</pre>';
+// });
+
+$router->run();
---
+++ public/noyau/classes/Router.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+    ./noyau/classes/App.php
+    Classe de l'application
+
+    routeur trouvé sur: https://github.com/obt-development/P5-ROUTER-POO
+  */
+
+namespace Noyau\Classes;
+
+
+/**
+ * @property  url
+ */
+class Router
+{
+  private $url;
+  private $routes = [];
+  private $namedRoute = [];
+
+  public function __construct($url)
+  {
+    $this->url = $url;
+  }
+
+  public function get($path, $callable, $name = null)
+  {
+    return $this->add($path, $callable, $name, 'GET');
+  }
+
+  public function post($path, $callable, $name = null)
+  {
+    return $this->add($path, $callable, $name, 'POSTS');
+  }
+
+  private function add($path, $callable, $name, $method)
+  {
+    $route = new Route($path, $callable);
+    $this->routes[$method][] = $route;
+    if (is_string($callable) && $name === null) {
+      $name = $callable;
+    }
+    if ($name) {
+      $this->namedRoute[$name] = $route;
+    }
+    return $route;
+  }
+
+  public function run()
+  {
+    if (!isset($this->routes[$_SERVER['REQUEST_METHOD']])) {
+      throw new RouteException('REQUEST_METHOD does not exist');
+    }
+    foreach ($this->routes[$_SERVER['REQUEST_METHOD']] as $route) {
+      // print_r($this->namedRoute);
+      if ($route->match($this->url)) {
+        return $route->call();
+      }
+
+    }
+
+    throw new RouterException('No matching routes');
+   
+
+  }
+
+  public function url($name, $params = [])
+  {
+    if (!isset($this->namedRoute[$name])) {
+      throw new RouterException('No route matches this name');
+    }
+    return $this->namedRoute[$name]->getUrl($params);
+  }
+}
